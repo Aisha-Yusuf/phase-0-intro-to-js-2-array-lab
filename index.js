90d6be79591b8296a29d592e2c3c13197a6777bf
Aisha-Yusuf <ayshaofficiel@gmail.com>
2023-09-24 17:44:20 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(paka) {
+  cats.push(paka);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(kitty) {
+  const newCats = [...cats, kitty];
+  return newCats;
+}
+
+function prependCat(kitten) {
+  const newCats = [kitten, ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = cats.slice(0,-1);
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  return newCats;
+}
